c2a4f87a5ad86b9e24ccc8726cf542faa84dfa6b
Edward Fernandez <edwardfernandez26@gmail.com>
2015-06-06 13:13:02 -0430

    Script que permite destruir tanto el personaje como la plataformas.

---
+++ Assets/Scripts/Destructor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructor : MonoBehaviour {
+
+	// .------------------------------------------------------------------------------------------------------------.
+
+	void Start () {
+	
+	}
+
+	// .------------------------------------------------------------------------------------------------------------.	
+	
+	void Update () {
+	
+	}
+
+	// .------------------------------------------------------------------------------------------------------------.
+
+	// Cuando entre en contacto con el objeto destructor se ejecutará este método.
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player"){
+			// Se termina la ejecución del juego.
+			Debug.Break();
+
+			// AQUÍ VAN LAS COSAS QUE SUCEDERÁN DESPUÉS DE
+			// QUE HAYA PERDIDO EL JUGADOR.
+		
+		}else{
+			Destroy(other.gameObject);	
+		}
+	}
+
+	// .------------------------------------------------------------------------------------------------------------.
+
+}
