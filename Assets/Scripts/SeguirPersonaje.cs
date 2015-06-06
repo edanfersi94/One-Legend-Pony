6489fd67c5dee3de67b64e3889ce2203facb1b3b
Edward Fernandez <edwardfernandez26@gmail.com>
2015-06-06 13:15:08 -0430

    Script que permite que la camara siga al personaje.

--- Assets/Scripts/SeguirPersonaje.cs
+++ Assets/Scripts/SeguirPersonaje.cs
@@ -4,14 +4,21 @@ using System.Collections;
 public class SeguirPersonaje : MonoBehaviour {
 
 	public Transform personaje;
-	public float separacion = 6f;
+	public float separacion = 0f;
+
+	// .------------------------------------------------------------------------------------------------------------.
 
 	void Start () {
 	
 	}
-	
+
+	// .------------------------------------------------------------------------------------------------------------.
+
 	// Función que se ejecutará en cada frame.
 	void Update () {
 		transform.position = new Vector3(personaje.position.x + separacion, transform.position.y, transform.position.z);
 	}
+
+	// .------------------------------------------------------------------------------------------------------------.
+
 }
